3074f6ac2866f1dcc0d073dba1f8db5d6a5dfe4b
Pedro B. de Quadros <quadros.pedro@pucpr.edu.br>
2024-05-13 14:03:10 -0300

    Exemplo de uso do Classificador

--- TCC_examples/plot_dissimilarity.py
+++ TCC_examples/plot_dissimilarity.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 sys.path.insert(0, '/home/pedro/projects/dataset_arff_covert')
 from arffconvert import load_SEAGenerator # Carrega a base de dados que está no formato arff para numpy X e Y
-######################
+##############################
 
 # SELEÇÃO DO MODELO
 from sklearn import model_selection
@@ -20,15 +20,15 @@ from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_
 # Classificador usando ESPAÇO DE DISSIMILARIDADE COM SELEÇÃO DOS R ALEATÓRIOS
 from sklearn.dissimilarity import DissimilarityRNGClassifier
 
-# data = load_iris()
+data = load_iris()
 # data = load_digits()
-# X, y = data.data, data.target
+X, y = data.data, data.target
 
-X, y = load_SEAGenerator() # Usando o arff | ESSE PROCESSO DEMORA UM POUCO, ELE CONVERTE O ARFF PARA UM CONJUNTO X e y, onde o X é as features e o Y é as classes
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=42)
+# X, y = load_SEAGenerator() # Usando o arff | ESSE PROCESSO DEMORA UM POUCO, ELE CONVERTE O ARFF PARA UM CONJUNTO X e y, onde o X é as features e o Y é as classes
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=60, shuffle=False)
 
 # TODO: PERGUNTA: Qual a qauntidade ideal de instancias R para montar o espaço de dissmilaridade?
-dissimilarity = DissimilarityRNGClassifier(estimator=KNeighborsClassifier(), random_state=42)
+dissimilarity = DissimilarityRNGClassifier(estimator=KNeighborsClassifier(), random_state=60)
 print(dissimilarity) # Mostra o teste que estamos usando
 # dissimilarity = GaussianNB()
 dissimilarity.fit(X_train, y_train)
